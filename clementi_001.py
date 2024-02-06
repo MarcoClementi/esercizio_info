a2bf84d611e45bb26371c32d9027bdc23f8a0e04
John Doe <marco.clementi@studenti.isissgobetti.it>
2024-02-06 11:26:03 +0100

    macio

--- clementi_001.py
+++ clementi_001.py
@@ -1,37 +1,34 @@
 #Chiesti all’utente 5 voti in input dire quanti di questi sono sufficienti su quelli 
 # considerati validi. I voti sono considerati validi se compresi tra 1 e 10.
 voto_1 = int(input("Inserisci il voto: "))
-voto_2 = int(input("Insersci il voto:  "))
+voto_2 = int(input("Inserisci il voto: "))
 voto_3 = int(input("Inserisci il voto: "))
 voto_4 = int(input("Inserisci il voto: "))
 voto_5 = int(input("Inserisci il voto: "))
 
-if( voto_1 >= 1 and voto_1 <= 10)   #voto_2 >= 1 and voto_2 <=10 and voto_3>= 1 and  voto_3<= 10 and  voto_4>=1 and  voto_4<=10 and  voto_5>=1 and  voto_5<=10 )
-print("il numero è valido "):
+valid_range = range(1, 11)  # Valid range for votes is from 1 to 10
 
+if voto_1 in valid_range:
+    print("Il numero è valido.")
 else:
-    print("il numero non è valido")
+    print("Il numero non è valido.")
 
-if( voto_2 >= 1 and voto_2 <=10 ):
-print("il numero è valido ")
-
+if voto_2 in valid_range:
+    print("Il numero è valido.")
 else:
-print("il numero non è valido")
+    print("Il numero non è valido.")
 
-if(voto_3>= 1 and  voto_3<= 10):
-print("il numero è valido ")
-
+if voto_3 in valid_range:
+    print("Il numero è valido.")
 else:
-    print("il numero non è valido")
+    print("Il numero non è valido.")
 
-if( voto_4>=1 and  voto_4<=10)
-print("il numero è valido")
-
+if voto_4 in valid_range:
+    print("Il numero è valido.")
 else:
-    print("il numero non è valido")
+    print("Il numero non è valido.")
 
-       if(voto_5>=1 and voto_5<= 10)
-       print("il numero è valido")
-       
-       else:
-        print("il numero non è valido")
+if voto_5 in valid_range:
+    print("Il numero è valido.")
+else:
+    print("Il numero non è valido.")
